9f96e54895279fd59e4ad298303ab597bb36e28a
Lara-Amelia <laraameliamf@gmail.com>
2025-06-08 22:47:02 -0300

    fim da implementação da coloração gulosa do grafo

--- colorGul.jl
+++ colorGul.jl
@@ -1,6 +1,6 @@
-#funções que alteram os argumentos recebidos tem ! no nome, por convenção
-#algoritmo de coloração guloso - apenas não colocaremos a mesma cor para vértices adjacentes
-#são usadas no máximo num_vertices cores
+# funções que alteram os argumentos recebidos tem ! no nome, por convenção
+# algoritmo de coloração guloso - apenas não colocaremos a mesma cor para vértices adjacentes
+# são usadas no máximo num_vertices cores
 
 #le as informações inciais do grafo: número de vértices e de arestas
 function leInfo!(nome_arquivo)
@@ -20,9 +20,8 @@ function leInfo!(nome_arquivo)
     return num_vertices, num_arestas
 end
 
-#le as linhas com infos sobre arestas e popula a matriz de adjacência
+# le as linhas com infos sobre arestas e popula a matriz de adjacência
 function leArestas!(nome_arquivo, matriz_adj)
-    u = v = 0
     open(nome_arquivo, "r") do arquivo
         for linha in eachline(arquivo)
             linha = strip(linha)
@@ -30,32 +29,44 @@ function leArestas!(nome_arquivo, matriz_adj)
                 partes = split(linha)
                 u = parse(Int, partes[2])
                 v = parse(Int, partes[3])
-                matriz_adj[u, v] = 1
-                matriz_adj[v, u] = 1
+                matriz_adj[u, v] = 1 
+                matriz_adj[v, u] = 1 
             end
         end
     end
 end
 
-function coloracao!(matriz_adj, cores_vertices)
-#=
-    ideia inicial: 
-    inicializamos um array de tamanho num_vertices com -1 em todas as posições, 
-    indicando que ainda não foi atribuída cor para os vértices
-    os vértices são identificados pelo índice no vetor e o conteúdo que armazenam é o 
-    número da cor correspondente utilizada na coloração 
+function coloracao!(matriz_adj, cores_vertices, num_vertices)
 
-    para cada vértice, percorremos a coluna (ou linha) correspondente ao vértice checando adjacências 
-    e, se uma cor já foi utilizada por vértices adjacentes, eliminamos o número associado à cor 
-    das possibilidades de cor para o vértice 
-    
-    utilizar algum tipo de registro (por exemplo, um array de valores booleanos) de forma a eliminar 
-    os valores específicos encontrados e, após a busca, atribuir ao vértice atual o menor valor 
-    "disponível" no registro
-=#
+    #o array 'cores_disponiveis_para_vertice' deve ser resetado para TRUE para CADA vértice que estamos colorindo
+    #ele deve ter tamanho suficiente para as cores (de 1 a num_vertices, já que o máximo de cores é o nro de vértices em si)
+    cores_disponiveis_para_vertice = Vector{Bool}(undef, num_vertices + 1)
+
+    #itera sobre cada vértice para atribuir uma cor
+    for i in 1:num_vertices #i é o vértice atual que estamos tentando colorir
+        fill!(cores_disponiveis_para_vertice, true)
+
+        #checamos quais cores já foram usadas por vértices adjacentes ao i 
+        #e, portanto, não estão mais disponíveis
+        for j in 1:num_vertices 
+            if matriz_adj[i, j] == 1
+                if cores_vertices[j] != -1 && cores_vertices[j] <= num_vertices
+                    cor_do_adjacente = cores_vertices[j]
+                    cores_disponiveis_para_vertice[cor_do_adjacente] = false
+                end
+            end
+        end
+
+        #atribui a menor cor disponível ao vértice i analisado
+        for k in 1:num_vertices 
+            if cores_disponiveis_para_vertice[k] == true 
+                cores_vertices[i] = k 
+                break
+            end
+        end
+    end
 end
 
-# "main"
 println("Insira o nome do arquivo a ser lido: ")
 nome_arquivo = readline()
 
@@ -63,9 +74,23 @@ num_vertices, num_arestas = leInfo!(nome_arquivo)
 println("nro de vertices: $num_vertices")
 println("nro de arestas: $num_arestas")
 
-#inicializa a matriz de adjacência de dimensões num_vertices com zeros 
-#e depois a inicializa com as infos lida do arquivo sobre arestas 
+#inicializa a matriz de adjacência de dimensões num_vertices com zeros
+#e depois a inicializa com as infos lida do arquivo sobre arestas
 matriz_adj = zeros(Int, num_vertices, num_vertices)
 leArestas!(nome_arquivo, matriz_adj)
-println("Matriz de adjacência inicializada com arestas:")
-println(matriz_adj)
+#println("Matriz de adjacência inicializada com arestas:")
+#println(matriz_adj) 
+
+#cria o array com entradas -1 e tam = num_vertices
+#-1 indica que ainda não foi atribuída cor para o vértice
+cores_vertices = fill(-1, num_vertices)
+
+coloracao!(matriz_adj, cores_vertices, num_vertices)
+
+println("\nResultados da Coloração:")
+for i in 1:num_vertices
+    println("Cor do vértice $i: $(cores_vertices[i])") 
+end
+
+nro_cores = maximum(cores_vertices)
+println("O número total de cores usado foi: $nro_cores")
