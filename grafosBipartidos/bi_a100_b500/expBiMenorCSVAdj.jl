64745efc671e77c9a55a8acfd659b4c77eb624e8
Lara-Amelia <laraameliamf@gmail.com>
2026-01-17 01:16:22 -0300

    alteração no script para a = 100, b = 500

--- grafosBipartidos/bi_a100_b500/expBiMenorCSVAdj.jl
+++ grafosBipartidos/bi_a100_b500/expBiMenorCSVAdj.jl
@@ -264,7 +264,7 @@ function main()
     N_POP = 200
     
     TARGET_A = 100
-    TARGET_B = 100
+    TARGET_B = 500
 
     # filtragem de arquivos de entrada de acordo com os parâmetros desejados
     all_files = filter(f -> endswith(f, ".col"), readdir())
@@ -332,7 +332,7 @@ function main()
     col_order = [:a, :b, :N, :p, :M, :v, :mean_time, :se_time, :mean_chi, :se_chi, :instancia]
     select!(df_main, col_order)
 
-    CSV.write("results_GA_Final_a100_b100_final.csv", df_main)
+    CSV.write("results_GA_Final_a100_b500_final.csv", df_main)
 
     println("\n--- Experimentos concluídos ---")
 end
