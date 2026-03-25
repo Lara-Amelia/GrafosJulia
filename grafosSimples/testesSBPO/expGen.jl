07ab5af1ed7c3362084bb98bacb28e48b5c01d35
Lara-Amelia <laraameliamf@gmail.com>
2026-03-25 12:16:39 -0300

    testes com genéticos em grafos simples utilizando stag = 20 e pmutation = 0.8

--- grafosSimples/testesSBPO/expGen.jl
+++ grafosSimples/testesSBPO/expGen.jl
@@ -34,7 +34,7 @@ end
                  last_best = -1, stag_iters = 0) =
     CustomGAParams(N, p_mutation, stag_limit, last_best, stag_iters)=#
 
-function CustomGAParams(; N = 100, p_mutation = 0.5, stag_limit = 50, 
+function CustomGAParams(; N = 100, p_mutation = 0.8, stag_limit = 20, 
                          last_best = Inf, stag_iters = 0)
     selection_strategy = Metaheuristics.TournamentSelection(K=2, N=N)
     return CustomGAParams(N, p_mutation, stag_limit, last_best, stag_iters, selection_strategy)
@@ -72,7 +72,7 @@ end
 function graph_swap_mutation!(Q::AbstractMatrix{Float64})
     n_individuals, n_genes = size(Q)
     # adj_list = ADJ
-    p = 0.5 # probabilidade de ocorência de mutação nos filhos do crossover
+    p = 0.8 # probabilidade de ocorência de mutação nos filhos do crossover
 
     # "pré-seleção" dos filhos que serão mutados
     to_mutate = findall(rand(n_individuals) .< p)
@@ -236,11 +236,11 @@ function main()
 
     # configuração dos testes
     N_REPETITIONS = 5
-    K_STAG = 50
+    K_STAG = 20
     N_POP = 100
     SAVE_EVERY = 10  # frequência de limpeza da memória e salvamento no disco
     
-    csv_path = "results_GA_pmutation0.5_stag50_progresso.csv"
+    csv_path = "results_GA_pmutation0.8_stag20_progresso.csv"
 
     # localização e filtragem
     raiz_grafos = dirname(@__DIR__) 
